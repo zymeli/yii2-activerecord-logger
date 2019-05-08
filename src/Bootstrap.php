2835609b2ef773e12205a3b20b20cb15cc6fab15
zymeli <710055@qq.com>
2019-05-08 16:16:13 +0800

    Bootstrap

---
+++ src/Bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace zymeli\ActiveRecordLogger;
+
+/**
+ * Class Bootstrap
+ * @package zymeli\ActiveRecordLogger
+ */
+class Bootstrap implements \yii\base\BootstrapInterface
+{
+    /**
+     * @param \yii\base\Application $app
+     */
+    public function bootstrap($app)
+    {
+
+    }
+}
